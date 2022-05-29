1a3492f782d2fb8533adc29bf416aab9cf394397
Mostafa Ahmed <mostafa.ah.abdelaziz@gmail.com>
2022-05-29 21:54:01 +0200

    Procfile & server setup

--- app.py
+++ app.py
@@ -2,4 +2,8 @@ from maindash import app
 from dashboard_layout import generate_layout
 
 app.layout = generate_layout()
-app.run_server(debug=True, dev_tools_hot_reload=True)
+
+server = app.server
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
